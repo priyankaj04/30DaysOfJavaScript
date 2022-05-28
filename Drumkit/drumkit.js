e30f61732053593ae7124d7218d1d6b8b9564921
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-05-28 19:11:32 +0530

    Update drumkit.js

--- Drumkit/drumkit.js
+++ Drumkit/drumkit.js
@@ -1,10 +1,18 @@
-var ch = "aosiruoiroah";
+const playSound = (e) => {
+        const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key= "${e.keyCode}"]`)
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+};
 
-console.log("this is char ", ch);
-www = () => {
-        console.log("hello");
+function removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
 }
 
-www();
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
 
-console.log("What is wrong with this?");
+window.addEventListener('keydown', playSound);
